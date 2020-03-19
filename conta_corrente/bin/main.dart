82c509afdb9ab44f667295ea4a0d9f0953ef8a84
danvb112 <danielbonasser@gmail.com>
2020-03-19 01:23:43 -0300

    Módulo 2

--- conta_corrente/bin/main.dart
+++ conta_corrente/bin/main.dart
@@ -1,5 +1,25 @@
-import 'package:conta_corrente/conta_corrente.dart' as conta_corrente;
+void main() {
 
-void main(List<String> arguments) {
-  print('Hello world: ${conta_corrente.calculate()}!');
+  var conta = new ContaCorrente();
+
+  conta.titular = "Daniel";
+  conta.agencia = 123;
+  conta.conta = 1234;
+  conta.saldo = 150.0;
+
+  print("O nome do titular é: ${conta.titular}");
+  print("O número da Agencia é: ${conta.agencia}");
+  print("O número da conta é: ${conta.conta}");
+  print("O saldo é: ${conta.saldo}");
+
+  print(conta);
+}
+
+class ContaCorrente {
+  String titular;
+  int agencia;
+  int conta;
+  double saldo;
+
+  
 }
